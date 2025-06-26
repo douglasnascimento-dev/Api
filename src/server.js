a52371f66dfa9632a4b193a03068abed79489632
Douglas <douglasnascimentomatos06@gmail.com>
2025-06-25 22:24:31 -0300

    feat: enhance server startup logs with port and access URL information

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 import app from './app.js';
 
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT, () => {
+  console.log();
+  console.log(`Servidor escutando na porta ${process.env.APP_PORT}`);
+  console.log(`Acesse em http://localhost:${process.env.APP_PORT}`);
+});
