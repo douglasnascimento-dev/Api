87aa0aa5ae65a0167e6bfd0aaa8141beb9b2518b
Douglas <douglasnascimentomatos06@gmail.com>
2025-06-25 22:27:33 -0300

    fix: correct helmet middleware usage in app.js and enhance server startup logs

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,7 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
 
-_appjs2.default.listen(process.env.APP_PORT);
+_appjs2.default.listen(process.env.APP_PORT, () => {
+  console.log();
+  console.log(`Servidor escutando na porta ${process.env.APP_PORT}`);
+  console.log(`Acesse em http://localhost:${process.env.APP_PORT}`);
+});
