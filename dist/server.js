02e9c091143c10b29dbd6852fb5732bae34f4b9c
Douglas <douglasnascimentomatos06@gmail.com>
2025-06-25 22:30:41 -0300

    fix: remove server startup logs for port and access URL in server.js

--- dist/server.js
+++ dist/server.js
@@ -2,6 +2,4 @@
 
 _appjs2.default.listen(process.env.APP_PORT, () => {
   console.log();
-  console.log(`Servidor escutando na porta ${process.env.APP_PORT}`);
-  console.log(`Acesse em http://localhost:${process.env.APP_PORT}`);
 });
--- src/server.js
+++ src/server.js
@@ -2,6 +2,4 @@ import app from './app.js';
 
 app.listen(process.env.APP_PORT, () => {
   console.log();
-  console.log(`Servidor escutando na porta ${process.env.APP_PORT}`);
-  console.log(`Acesse em http://localhost:${process.env.APP_PORT}`);
 });
