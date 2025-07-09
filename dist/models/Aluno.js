31f6f31ae8e2885756a6973bfc0c8aa8d9df45f6
Douglas <douglasnascimentomatos06@gmail.com>
2025-07-09 11:18:54 -0300

    fix: alteração da lógica de validação de idade e altura para alunos

--- dist/models/Aluno.js
+++ dist/models/Aluno.js
@@ -5,67 +5,85 @@ var _constantsjs = require('../constants/constants.js');
   static init(sequelize) {
     super.init(
       {
-        name: {
-          type: _sequelize2.default.STRING,
-          defaultValue: '',
-          allowNull: false,
-          validate: {
-            len: {
-              args: [_constantsjs.MIN_NAME_LENGTH, _constantsjs.MAX_NAME_LENGTH],
-              msg: 'O nome deve ter entre 3 e 255 caracteres',
-            },
-          },
+      name: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        allowNull: false,
+        validate: {
+        len: {
+          args: [_constantsjs.MIN_NAME_LENGTH, _constantsjs.MAX_NAME_LENGTH],
+          msg: 'O nome deve ter entre 3 e 255 caracteres',
         },
-        sobrenome: {
-          type: _sequelize2.default.STRING,
-          defaultValue: '',
-          allowNull: false,
-          validate: {
-            len: {
-              args: [_constantsjs.MIN_NAME_LENGTH, _constantsjs.MAX_NAME_LENGTH],
-              msg: 'O sobrenome deve ter entre 3 e 255 caracteres',
-            },
-          },
-        },
-        email: {
-          type: _sequelize2.default.STRING,
-          allowNull: false,
-          defaultValue: '',
-          unique: true,
-          validate: {
-            isEmail: {
-              msg: 'O email deve ser um endereço de email válido',
-            },
-          },
-        },
-        idade: {
-          type: _sequelize2.default.INTEGER,
-          defaultValue: 0,
-          allowNull: false,
-          validate: {
-            isInt: {
-              msg: 'A idade deve ser um número inteiro',
-            },
-            min: {
-              args: [0],
-              msg: 'A idade não pode ser negativa',
-            },
-          },
-        },
-        peso: {
-          type: _sequelize2.default.FLOAT,
-          defaultValue: 0.0,
-          allowNull: false,
-        },
-        altura: {
-          type: _sequelize2.default.FLOAT,
-          defaultValue: 0.0,
-          allowNull: false,
         },
       },
+      sobrenome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        allowNull: false,
+        validate: {
+        len: {
+          args: [_constantsjs.MIN_NAME_LENGTH, _constantsjs.MAX_NAME_LENGTH],
+          msg: 'O sobrenome deve ter entre 3 e 255 caracteres',
+        },
+        },
+      },
+      email: {
+        type: _sequelize2.default.STRING,
+        allowNull: false,
+        defaultValue: '',
+        unique: true,
+        validate: {
+        isEmail: {
+          msg: 'O email deve ser um endereço de email válido',
+        },
+        },
+      },
+      idade: {
+        type: _sequelize2.default.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+        validate: {
+        isInt: {
+          msg: 'A idade deve ser um número inteiro',
+        },
+        min: {
+          args: [0],
+          msg: 'A idade não pode ser negativa',
+        },
+        },
+      },
+      peso: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: 0.0,
+        allowNull: false,
+        validate: {
+        isFloat: {
+          msg: 'O peso deve ser um número',
+        },
+        min: {
+          args: [0],
+          msg: 'O peso não pode ser negativo',
+        },
+        },
+      },
+      altura: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: 0.0,
+        allowNull: false,
+        validate: {
+        isFloat: {
+          msg: 'A altura deve ser um número',
+        },
+        min: {
+          args: [0],
+          msg: 'A altura não pode ser negativa',
+        },
+        },
+      },
+      },
       {
-        sequelize,
-        tableName: 'alunos',
+      sequelize,
+      tableName: 'alunos',
       },
     );
 
